90116351cc52bb4ad769ab4c71f2c1064beca037
hfzespinoz4 <harvin.zuniga@gmail.com>
2019-07-28 15:21:25 +0200

    Overlaping en banner del Landing Page

--- Proyecto JS/js/app.js
+++ Proyecto JS/js/app.js
@@ -18,8 +18,49 @@ const navSlide = () => {
         //Burger animation
         burger.classList.toggle('toggle');
     });
-
-    
 } 
-
 navSlide();
+
+//Carga del slde del Main Page
+const services = [
+  'Soluciones IT a su medida',
+  'Infraestructura y Servicios de Alta Disponibilidad',
+  'Soluciones de Retail',
+]
+
+const images = [
+  './media/bn-it.jpg',
+  './media/bn-nube.jpg',
+  './media/bn-retail.jpg',
+]
+
+const promos = [
+  'Tenemos la solución para cada reto tecnologico de su empresa',
+  'Tu empresa segura y accesible desde cualquier lugar',
+  'Software, Servicios y Asesoría para el sector retail',
+]
+
+let currentSlide = 0;
+const image = document.querySelector('#banner-images');
+const promo = document.querySelector('#serv-description');
+const serv = document.querySelector('#h1');
+image.setAttribute('src', images[currentSlide]);
+serv.innerHTML = services[currentSlide];
+promo.innerHTML = promos[currentSlide];
+
+const updateBanner = () => {
+    console.log(currentSlide);
+    if ((currentSlide +1) < images.length ){
+        currentSlide = currentSlide + 1;
+    } else {
+        currentSlide = 0;
+    }
+    console.log(currentSlide);
+    image.setAttribute('src', images[currentSlide]);
+    serv.innerHTML = services[currentSlide];
+    promo.innerHTML = promos[currentSlide];
+};
+
+window.setInterval(updateBanner, 5000);
+
+updateBanner();
